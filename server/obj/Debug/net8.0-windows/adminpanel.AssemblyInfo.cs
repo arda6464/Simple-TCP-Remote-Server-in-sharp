daec33f57403f088c86bc22057ba9b2762f797c9
arda64 <166497625+arda6464@users.noreply.github.com>
2025-07-07 23:08:46 +0300

    birleştirildi

--- server/obj/Debug/net8.0-windows/adminpanel.AssemblyInfo.cs
+++ server/obj/Debug/net8.0-windows/adminpanel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("adminpanel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61587d73bb486694a9ee6c069a278455be92b719")]
 [assembly: System.Reflection.AssemblyProductAttribute("adminpanel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("adminpanel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
